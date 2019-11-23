c02339f27b2ad30b080544bb996e22326c9ad4d4
nyroro <359779828@qq.com>
2019-11-23 19:15:29 +0800

    WGAN-GP on cifar

---
+++ WGAN-GP_cifar10/result/chart.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import re
+
+ptn = re.compile(r'(\d+) \[D loss: ([\d\-\.]+)\] \[G loss: ([\d\-\.]+)\]')
+datas = []
+with open('result.txt', 'r') as f:
+    for line in f:
+        result = ptn.match(line)
+        if result != None:
+            datas.append((int(result.group(1)), float(
+                result.group(2)), float(result.group(3))))
+
+datas = sorted(datas, key=lambda t: t[0])
+fig, ax = plt.subplots()
+plt.plot([t[0] for t in datas], [t[1] for t in datas], label='D')
+plt.plot([t[0] for t in datas], [t[2] for t in datas], label='G')
+plt.legend()
+plt.savefig('loss.png')
